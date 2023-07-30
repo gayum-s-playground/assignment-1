acdc1ee3d170eeba9e7e6a830091dd4387ac6abf
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-07-30 18:23:34 +0530

    Complete task 6-C

--- DemoTaskC.java
+++ DemoTaskC.java
@@ -1,15 +1,15 @@
 public class DemoTaskC {
     public static void main(String[] args) {
 
-        final String BOLD_TEXT="\033[30;1m";
+        final String BOLD_TEXT="\033[37;1m";
         final String RESET_TEXT="\033[0m";
         
-        System.out.printf("+%15s+%10s+%10s+\n","---------------","-----------","-------------");
-        System.out.printf("|%1$sCITY%2$15s|%1$sSTATE%2$10s|%1$sPOPULATION%2$7s|\n",BOLD_TEXT,RESET_TEXT);
-        System.out.printf("+%15s+%10s+%10s+\n","---------------","-----------","-------------");
-        System.out.printf("|%s%5s|%s%1s|%,d%4s|\n","Los Angles","","California","",3966936,"");
-        System.out.printf("|%s%7s|%s%3s|%,d%4s|\n","New York","","New York","",8336817,"");
-        System.out.printf("+%15s+%10s+%10s+\n","---------------","-----------","-------------");
+        System.out.printf("\033[33m+%15s+%10s+%7s+\033[0m\n","---------------","-----------","-----------");
+        System.out.printf("\033[33m|\033[0m%1$sCITY%2$15s\033[33m|\033[0m%1$sSTATE%2$10s\033[33m|\033[0m%1$sPOPULATION%2$5s\033[33m|\033[0m\n",BOLD_TEXT,RESET_TEXT);
+        System.out.printf("\033[33m+%15s+%10s+%7s+\033[0m\n","---------------","-----------","-----------");
+        System.out.printf("\033[33m|\033[0m%s%5s\033[33m|\033[0m%s%1s\033[33m|\033[0m\033[35m%,d%2s\033[0m\033[33m|\033[0m\n","Los Angles","","California","",3966936,"");
+        System.out.printf("\033[33m|\033[0m\033[32m%s\033[0m%s%7s\033[33m|\033[0m\033[32m%s\033[0m%s%3s\033[33m|\033[0m\033[35m%,d%2s\033[0m\033[33m|\033[0m\n","New ","York","","New ","York","",8336817,"");
+        System.out.printf("\033[33m+%15s+%10s+%7s+\033[0m\n","---------------","-----------","-----------");
 
     }
     

1a1148e3eda12f05bb17f68d594b0fa919f52c83
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-07-30 18:27:02 +0530

    Complete task 6-D

--- DemoTaskD.java
+++ DemoTaskD.java
@@ -6,10 +6,10 @@ public class DemoTaskD {
 
         System.out.printf("\033[33m+%s+%s+%s+\033[0m\n","-".repeat(7),"-".repeat(15),"-".repeat(7));
         System.out.printf("\033[33m|\033[0m%1$sITEM%2$s\t\033[33m|\033[0m%1$sQUANTITY%2$s\t\033[33m|\033[0m%1$sPRICE%2$s\t\033[33m|\033[0m\n",BOLD_TEXT,RESET);
-        System.out.printf("\033[33m+%s+%s+%s+\033[0m\n","-".repeat(7),"-".repeat(15),"-".repeat(7));
+        System.out.printf("\033[33m+\033[32m%s\033[33m+%s+\033[32m%s\033[33m+\033[0m\n","-".repeat(7),"-".repeat(15),"-".repeat(7));
         System.out.printf("\033[33m|\033[0m%s\t\033[33m|\033[0m\033[34m%15d\033[0m\033[33m|\033[0m\033[32m$\033[0m\033[31m%.2f\033[0m\t\033[33m|\033[0m\n","Apples",5,0.99);
         System.out.printf("\033[33m|\033[0m%s\033[33m|\033[0m\033[34m%15d\033[0m\033[33m|\033[0m\033[32m$\033[0m\033[31m%.2f\033[0m\t\033[33m|\033[0m\n","Oranges",10,1.49);
-        System.out.printf("\033[33m+%s+%s+%s+\033[0m\n","-".repeat(7),"-".repeat(15),"-".repeat(7));
+        System.out.printf("\033[33m+\033[32m%s\033[33m+%s+\033[32m%s\033[33m+\033[0m\n","-".repeat(7),"-".repeat(15),"-".repeat(7));
 
     }
     

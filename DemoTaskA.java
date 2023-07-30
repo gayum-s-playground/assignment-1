f89d21861aaac391f426eaaa51a38f9d022160a6
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-07-30 16:16:52 +0530

    Complete task 6-A correction

--- DemoTaskA.java
+++ DemoTaskA.java
@@ -1,6 +1,6 @@
 public class DemoTaskA{
     public static void main(String[] args) {
      
-        System.out.printf("\033[31m[% 5d\033[32m%s%5s]\n",5,"0%"," ");
+        System.out.printf("\033[41m[% 5d\033[42m%s%5s]\033[0m\n",5,"0%"," ");
     }
 }
---
+++ DemoTaskD.java
@@ -0,0 +1,8 @@
+public class DemoTaskD {
+    public static void main(String[] args) {
+        
+        final String BOLD_TEXT="\033[30;1m";
+        final String COLOR_BLUE="\033[34m";
+    }
+    
+}

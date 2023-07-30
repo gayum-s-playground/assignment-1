23da6da68e33c3c615833cfe54a14d2a1706a734
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-07-30 18:03:17 +0530

    Complete task 6-B

--- DemoTaskB.java
+++ DemoTaskB.java
@@ -4,12 +4,12 @@ public class DemoTaskB {
         final String COLOR="\033[34m";
         final String RESET="\033[0m";
        
-        System.out.printf("+%10s+%5s+\n","----------","-----");
-        System.out.printf("|%1$sNAME%2$10s|%1$sAGE%2$6s|\n",COLOR,RESET);
-        System.out.printf("+%10s+%5s+\n","----------","-----");
-        System.out.printf("|%-10s|%-5d|\n","Alice",24);
-        System.out.printf("|%-10s|%-5d|\n","Bob",30);
-        System.out.printf("+%10s+%5s+\n","----------","-----");
+        System.out.printf("\033[33m+%10s+%5s+\033[0m\n","----------","-----");
+        System.out.printf("\033[33m|\033[0m%1$sNAME%2$10s\033[33m|\033[0m%1$s AGE%2$5s\033[33m|\033[0m\n",COLOR,RESET);
+        System.out.printf("\033[33m+%10s+%5s+\033[0m\n","----------","-----");
+        System.out.printf("\033[33m|\033[0m%-10s\033[33m|\033[0m\033[35m%5d\033[0m\033[33m|\033[0m\n","Alice",24);
+        System.out.printf("\033[33m|\033[0m%-10s\033[33m|\033[0m\033[35m%5d\033[0m\033[33m|\033[0m\n","Bob",30);
+        System.out.printf("\033[33m+%10s+%5s+\033[0m\n","----------","-----");
 
     }
     
